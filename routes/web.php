22f86f095fd78b37b86437d7e70412575ac044c0
Xhentila <x.dautaj@brandmonks.de>
2023-12-23 22:41:50 +0100

    feat: api http facade

--- routes/web.php
+++ routes/web.php
@@ -14,5 +14,17 @@ use Illuminate\Support\Facades\Route;
 */
 
 Route::get('/', function () {
-    return view('welcome');
+    Illuminate\Support\Facades\Http::post('https://api.openai.com/v1/chat/completions', [
+        "model"=> "gpt-3.5-turbo",
+        "messages"=> [
+            [
+                "role"=> "system",
+                "content"=> "You are a poetic assistant, skilled in explaining complex programming concepts with creative flair."
+            ],
+            [
+                "role"=>"user",
+                "content"=> "Compose a poem that explains the concept of recursion in programming."
+            ]
+        ]
+    ]);
 });
